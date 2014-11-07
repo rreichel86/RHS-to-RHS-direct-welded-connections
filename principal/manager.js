7195fc1041530c9227e98d7fa66f77f06c465493
Rainer Ernst Reichel <rreichel86@gmail.com>
2014-11-06 19:07:31 -0600

    v0.6.0.5

--- principal/manager.js
+++ principal/manager.js
@@ -129,7 +129,7 @@ var Control = {
     },
     reporte_encabezado: ["----------------------------------------------------------------" +
                          "------------------------------------------------------------------",
-                         "RHS TO RHS DIRECT WELDED CONNECTIONS -- Beta version  v0.6.0.4",
+                         "RHS TO RHS DIRECT WELDED CONNECTIONS -- Beta version  v0.6.0.5,
                          "----------------------------------------------------------------" +
                          "------------------------------------------------------------------",
                          "Calculations are in conformance with the Load and Resistance Factor Design method (LRFD)",
--- principal/portada.js
+++ principal/portada.js
@@ -11,3 +11,20 @@ chrome.app.runtime.onLaunched.addListener(function () {
         }
     });
 });
+
+chrome.runtime.onUpdateAvailable.addListener(function (details) {
+    "use strict";
+    console.log("updating to version " + details.version);
+    chrome.runtime.reload();
+});
+
+chrome.runtime.requestUpdateCheck(function (status) {
+    "use strict";
+    if (status === "update_available") {
+        console.log("update pending...");
+    } else if (status === "no_update") {
+        console.log("no update found");
+    } else if (status === "throttled") {
+        console.log("Oops, I'm asking too frequently - I need to back off.");
+    }
+});
